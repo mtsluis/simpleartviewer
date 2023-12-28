5309c7a571e3c5cb649326441827e461cf517eb7
mtsluis <151209805+mtsluis@users.noreply.github.com>
2023-12-28 16:15:39 +0000

    created APIHandler, SearchHandler methods, and some main logic

---
+++ src/main/java/handlers/SearchHandler.java
@@ -0,0 +1,36 @@
+package handlers;
+
+import artwork.Artwork;
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+public class SearchHandler {
+
+    private int total;
+    private int[] objectIDs;
+    private Artwork[] artworksList;
+
+    public int getTotal() {
+        return total;
+    }
+
+    public int[] getObjectIDs() {
+        return objectIDs;
+    }
+
+    public Artwork[] getArtworksList() {
+        return artworksList;
+    }
+
+    public Artwork[] createArtList(APIHandler apiHandler, int searchLimit) throws URISyntaxException, IOException, InterruptedException {
+        Gson gson = new Gson();
+        artworksList = new Artwork[searchLimit];
+        for (int i = 0; i < searchLimit; i++) {
+            Artwork artwork = gson.fromJson(apiHandler.artworkRequest(String.valueOf(objectIDs[i])).body(), Artwork.class);
+            artworksList[i] = artwork;
+        }
+        return artworksList;
+    }
+}
---
+++ src/main/java/terminalprint/TerminalPrint.java
@@ -0,0 +1,4 @@
+package terminalprint;
+
+public class TerminalPrint {
+}
